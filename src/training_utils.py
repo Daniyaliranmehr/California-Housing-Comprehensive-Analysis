8eecea9ff0c06a0d7937b4a2cc482a0b18bc9c76
DaniyalIranMehr <daniyaliranmehr@gmail.com>
2026-06-05 22:25:53 +0330

    Define Trainer Class

--- src/training_utils.py
+++ src/training_utils.py
@@ -1,5 +1,10 @@
+import torch
+from rich.console import Console
+
 class AverageMeter(object):
-    """Computes and stores the average and current value"""
+    """
+    Computes and stores the average and current value
+    """
     def __init__(self):
         self.reset()
 
@@ -14,3 +19,62 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
+
+class Trainer:
+    """
+    Define the trainer with a specific loss function and optimizer
+    """
+    def __init__(self, model, optimizer, loss_fn):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        
+        self.train_history = []
+        self.valid_history = []
+
+    def train(self, train_loader):
+        self.model.train()
+        loss_train = AverageMeter()
+
+        for inputs, targets in train_loader:
+            outputs = self.model(inputs)
+            loss = self.loss_fn(outputs.squueze(), targets)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            loss_train.update(loss.item())
+
+        return loss_train.avg
+    
+    def validate(self, valid_loader):
+        self.model.eval()
+        loss_valid = AverageMeter()
+
+        with torch.no_grad():
+            for inputs, targets in valid_loader:
+                
+                outputs = self.model(inputs)
+                loss = self.loss_fn(outputs.squueze(), targets)
+
+                loss_valid.update(loss.item())
+
+        return loss_valid.avg
+    
+    def fit(self, train_loader, valid_loader, epochs=100, log_every=10):
+        for epoch in range(epochs):
+            train_loss = self.train_epoch(train_loader)
+            valid_loss = self.validate(valid_loader)
+
+            self.train_history.append(train_loss)
+            self.valid_history.append(valid_loss)
+
+            console = Console()
+
+            if epoch % log_every == 0:
+                console.print(f"[bold green]Epoch {epoch}[/bold green]")
+                console.print(f"Train Loss: {train_loss:.4f}")
+                console.print(f"Valid Loss: {valid_loss:.4f}")
+                console.print("")
